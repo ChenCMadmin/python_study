579c5625b23be1e9316ad28ac018f853df74bffa
86131 <760856329@qq.com>
2020-10-10 15:45:31 +0800

    基础语法

---
+++ practice/basis/git.py
@@ -0,0 +1,25 @@
+'''
+先自己注册github账号，本地安装git环境，打开Pycharm-File-Settings-Version control-Git设置页面
+填写git在本地的地址
+
+git--Add 提交文件
+共享给GitHub VCS--Import -github
+
+pull拉取代码
+找到VCS-Checkoutfrom Version Control-Git
+输入代码仓库地址
+
+更新代码
+打开Vcs-Git-Commit提交代码
+更新GitHub
+打开Vcs-Git-Push，勾选push标签为当前分支
+'''
+
+
+a = "abcdef"
+
+# 以 str 为分隔符切片 mystr，如果 maxsplit有指定值，则仅分隔 maxsplit 个子字符串
+ret5 = a.split("ad")
+print(ret5)
+print('test')
+print('test')
